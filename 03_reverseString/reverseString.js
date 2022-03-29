962f878c65f80ab0273248ed9a0b1efe3472c8ea
callmeog01 <ogb.official1@gmail.com>
2022-03-29 08:24:07 -0400

    Fixed and passed reverseString test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
 const reverseString = function() {
-
-};
+    const greeting = 'hello';
+    const word = greeting.split('')
+    word.reverse()
+    return word.join('');
+  }
+  reverseString()
 
 // Do not edit below this line
 module.exports = reverseString;
