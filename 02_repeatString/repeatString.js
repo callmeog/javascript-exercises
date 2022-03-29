571a3c51d7de05c596f363f72df2ac3f3068a29b
callmeog01 <ogb.official1@gmail.com>
2022-03-29 07:12:48 -0400

    Fixed repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
     let greeting = "";
+    while (num > 0) {
+        greeting += string;
+        num--;
+    }
+    return greeting;
 };
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
