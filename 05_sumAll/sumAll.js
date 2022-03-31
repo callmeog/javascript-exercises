91c90d3767fb24d1936d19076e26b2b141ee0812
callmeog01 <ogb.official1@gmail.com>
2022-03-31 08:41:16 -0400

    Passed sumALl num exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    let y = 2;
+    let x = 3;
+    for(let i = 0; i < 1; i++) {
+      const sum = y + x + a + b;
+      return sum;
+    }
+  };
+  sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
