2be4f99008e25c5cd65508ab5a0bfbc1483117fb
callmeog01 <ogb.official1@gmail.com>
2022-03-31 01:02:09 -0400

    Removed a single value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(a, b) {
+    a = [1, 2, 3, 4,], 3;
+    b = a.splice(2,1);
+    return a
+    
+  }
+  removeFromArray([1, 2, 3, 4,], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
