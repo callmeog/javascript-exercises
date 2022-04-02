0e21d428335f8da376794984d4ad2dcee0a55094
callmeog01 <ogb.official1@gmail.com>
2022-04-01 23:55:36 -0400

    Check if a given year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
+const leapYears = function(verifyLeapYear) {
+    if(verifyLeapYear / 4 && verifyLeapYear / 400) {
+      return true;
+    }else if(verifyLeapYear / 100) {
+      return false;
+    }
+  }
+  leapYears(1996)
 
 // Do not edit below this line
 module.exports = leapYears;
