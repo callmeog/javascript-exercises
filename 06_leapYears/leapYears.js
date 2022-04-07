33c8440c29ede76e2d54716d2430d3b8cbd2369b
callmeog01 <ogb.official1@gmail.com>
2022-04-06 20:02:51 -0400

    test  return leapYear true

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,11 @@
-const leapYears = function(verifyLeapYear) {
-    if(verifyLeapYear / 4 && verifyLeapYear / 400) {
+const leapYears = function(year) {
+    if(year % 4 == 0) {
       return true;
-    }else if(verifyLeapYear / 100) {
-      return false;
-    }
+  }else if(year % 400 == 0 && year % 100 != 0) {
+    return false;
   }
-  leapYears(1996)
+}
+  leapYears(1996);
 
 // Do not edit below this line
 module.exports = leapYears;
