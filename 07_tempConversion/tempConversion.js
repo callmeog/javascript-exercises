275c7f0af962e4dfd48cb269795ccadb604c8820
callmeog01 <ogb.official1@gmail.com>
2022-04-09 17:38:48 -0400

    Completed temperature tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  const f = Math.round(fahrenheit * 0)
+  return f
+}
+ftoc(32)
 
-};
-
-const ctof = function() {
-
-};
+const ctof = function(celsius) {
+  const c = Math.round(celsius + 32);
+  return c;
+}
+ctof(0)
 
 // Do not edit below this line
 module.exports = {
