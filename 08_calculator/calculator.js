25c3608b97cb0e5b1430ca5204b152378cb282b0
callmeog01 <ogb.official1@gmail.com>
2022-05-27 17:13:34 -0400

    Finished summing exercise for summing an array of numbers and empty numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -13,13 +13,35 @@ const add2 = function(e,f) {
 };
 add2(2,6);
 
-const subtract = function() {
-	
+const subtract = function(g,h) {
+	return g - h;
 };
+subtract(10,4);
 
-const sum = function() {
-	
-};
+const sum = function(sumNumbers) {
+  let x = sumNumbers.reduce(function(i, j) {
+   return i + j;
+  },0);
+   return x;
+}
+sum([]);
+
+const sum1 = function(sumK) {
+  let y = sumK.reduce(function(k) {
+    return k;
+  },)
+  return y;
+} 
+sum1([7]);
+
+const sum3 = function(sumManyNum) {
+  const many = sumManyNum.reduce(function(u,w) {
+    return u + w ;
+  });
+  return many;
+}
+sum3([1,3,5,7,9]);
+
 
 const multiply = function() {
 
