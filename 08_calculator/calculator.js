db4c73e5261b4b3adc20c79e9c45a971dbe20cdf
callmeog01 <ogb.official1@gmail.com>
2022-05-28 16:52:02 -0400

    sum numbers exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -12,11 +12,15 @@ const add2 = function(e,f) {
 	return e + f;
 };
 add2(2,6);
+//***** end of add *******/
+
 
 const subtract = function(g,h) {
 	return g - h;
 };
 subtract(10,4);
+//***** end of subtract *******/
+
 
 const sum = function(sumNumbers) {
   let x = sumNumbers.reduce(function(i, j) {
@@ -41,11 +45,25 @@ const sum3 = function(sumManyNum) {
   return many;
 }
 sum3([1,3,5,7,9]);
+//***** end of sum *******/
 
+const multiply = function(multiNum) {
+  let multiplyNum = multiNum.reduce(function(num1,num2) {
+    return num1 * num2;
+  });
+  return multiplyNum;
+}
+multiply([2,4])
 
-const multiply = function() {
+const multiplys = function(multiNums) {
+  let multiplyNums = multiNums.reduce(function(num3, num4) {
+    return num3 * num4;
+  });
+  return multiplyNums;
+}
+console.log(multiplys([2,4,6,8,10,12,14]));
+//***** end of multiply *******/
 
-};
 
 const power = function() {
 	
@@ -54,6 +72,7 @@ const power = function() {
 const factorial = function() {
 	
 };
+//***** end of factorial *******/
 
 // Do not edit below this line
 module.exports = {
